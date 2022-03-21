1585ad4cf8685c7f2770b935e6740a7a0034e37d
Yutaka <yutaka.urakami@colorado.edu>
2022-03-21 18:10:57 +0900

    finish mult.asm in chap4

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,37 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+// 高級言語版
+// R2 = 0;
+// while(0 < R1){
+//  R2 = R0 + R2
+//  R1 = R1 - 1
+// }
+//
+@R2
+M = 0 // R2 = 0
+
+@R1
+D=M
+@END
+D;JLE
+
+(LOOP)
+@R0
+D=M // D = R0
+
+@R2
+M=M+D // R2 = R2 + R0
+
+@R1
+M=M-1 // R1 = R1 - 1
+D = M
+
+
+@LOOP
+D;JGT
+
+(END)
+@END
+0;JMP
+
